78c7733d90c8f6ed7ca07016a34f22571902f5ff
Gonzaloherenu <gonzaherenu@gmail.com>
2021-09-21 18:31:00 -0300

    subo ultimas correciones de las observaciones

--- sprint2/app.js
+++ sprint2/app.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 const port = 3030
 
-app.listen(port, () => console.log('Servidor abierto en el puerto ' + port))
-
 app.use(express.static('public'))
 
 app.get('/', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'index.html'))})
-app.get('/index.html', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'index.html'))})
-app.get('/productDetail.html', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'productDetail.html'))})
-app.get('/productCart.html', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'productCart.html'))})
-app.get('/register.html', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'register.html'))})
-app.get('/login.html', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'login.html'))})
+app.get('/index', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'index.html'))})
+app.get('/productDetail', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'productDetail.html'))})
+app.get('/productCart', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'productCart.html'))})
+app.get('/register', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'register.html'))})
+app.get('/login', (rep, res) => {res.sendFile(path.join(__dirname, 'views' , 'login.html'))})
+
+app.listen(port, () => console.log('Servidor abierto en el puerto ' + port))
