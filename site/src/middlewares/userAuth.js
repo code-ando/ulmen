fab3a2249b857d56990006c3a25022bcde38b6c2
Gonzaloherenu <gonzaherenu@gmail.com>
2021-11-13 09:29:26 -0300

    subo observaciones de vista login

--- site/src/middlewares/userAuth.js
+++ site/src/middlewares/userAuth.js
@@ -1,7 +1,6 @@
 module.exports = (req, res, next) => {
-    if (req.session.usuarioLogueado !== undefined) {
-        next()
-    } else {
-        res.render('guestUser')
-    }
+    if (!req.session.usuarioLogueado) {
+        return res.redirect('/login')
+    } 
+    next()
 }
--- site/src/routes/user.js
+++ site/src/routes/user.js
@@ -2,10 +2,11 @@ const router = require('express').Router()
 
 
 /* const validate = require ("../middlewares/validate") */
-const {login, register, agregarUser, processLogin} = require('../controllers/userControllers')
+const {login, register, agregarUser, processLogin, profile, cerrarSesion} = require('../controllers/userControllers')
 const { json } = require('express')
 const imagenUser = require('../middlewares/userstorage')
 const guestUser = require('../middlewares/guestUser')
+const userAuth = require('../middlewares/userAuth')
 const validateRegister = require('../middlewares/validateRegister')
 
 //Register
@@ -16,5 +17,7 @@ router.post('/register', imagenUser.single('image'), validateRegister,agregarUse
 router.get('/login',guestUser, login)
 router.post('/login', processLogin)
 
+router.get('/profile', userAuth, profile)
+router.get('/cerrarSesion', cerrarSesion)
 
 module.exports = router
